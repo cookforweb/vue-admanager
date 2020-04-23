7afe2dd10db9ef28551d33aa6219baf9707a0d80
Thanasis Dimitriadis <thanasis.dimitriadis@mitsishotels.com>
2020-04-23 11:01:46 +0300

    Add image + Update readme + Add targeting criteria

--- components/AdmanagerSlot.vue
+++ components/AdmanagerSlot.vue
@@ -9,7 +9,13 @@
                 type: String,
                 required: true,
             },
+            targeting: {
+                type: Array,
+                required: false,
+                default: () => [],
+            },
             elementId: {
+                type: String,
                 default: () => 'admanager-cookforweb-' + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10),
                 required: false,
             }
@@ -35,17 +41,22 @@
 
         },
         methods: {
-            renderSlot(){
+            renderSlot() {
                 googletag.cmd.push(() => {
-                    this.defineSlot();
+                    // If default sizes is false then render as an out-of-page slot
+                    if (this.fetchDefaultSizes){
+                        this.defineSlot();
+                    } else {
+                        this.defineOutOfPageSlot();
+                    }
                     googletag.display(this.elementId);
                 });
             },
-            defineSlot(){
+            defineSlot() {
 
-                let slot = googletag.defineSlot(this.fullSlot, this.defaultSizes, this.elementId);
+                let slot = googletag.defineSlot(this.fetchSlot, this.fetchDefaultSizes, this.elementId);
 
-                if (this.slot.sizeMapping){
+                if (this.slot.sizeMapping) {
 
                     let sizeMapping = googletag.sizeMapping();
 
@@ -54,21 +65,67 @@
                     slot.defineSizeMapping(sizeMapping.build());
                 }
 
+                slot = this.setTargeting(slot);
+
                 return slot.addService(googletag.pubads());
+            },
+            defineOutOfPageSlot() {
+
+                let slot = googletag.defineOutOfPageSlot(this.fetchSlot, this.elementId).addService(googletag.pubads())
+
+                slot = this.setTargeting(slot);
+
+                return slot.addService(googletag.pubads());
+            },
+            // Targeting values for this specific slot
+            // Example of targeting object
+            // {
+            //     page: ['home', 'index'],
+            //     site: ['cookforweb.com'],
+            // }
+            setTargeting(slot){
+
+                if (this.fetchTargeting) {
+
+                    Object.keys(this.fetchTargeting).forEach((key) => {
+
+                        slot.setTargeting(key, this.fetchTargeting[key]);
+                    });
+                }
+                return slot;
+            }
+        },
+        computed: {
+            fetchSlot() {
+                return  `/${this.$admanager.account}/${this.slot.name}`;
+            },
+            fetchDefaultSizes() {
+                if (this.slot.fetchDefaultSizes){
+                    return this.slot.fetchDefaultSizes;
+                }
+                else {
+                    return false;
+                }
+            },
+            fetchTargeting() {
+                // If this slot has it's one targeting rules then use them
+                if (this.targeting){
+
+                    return this.targeting;
+                }
+                // else check if there are global rules defined
+                if (this.$admanager.targeting){
+
+                    return this.$admanager.targeting;
+                }
+
+                return false;s
             }
         },
         beforeDestroy () {
             googletag.cmd.push(() => {
                 googletag.destroySlots()
             })
-        },
-        computed: {
-            fullSlot(){
-                return  `/${this.$admanager.account}/${this.slot.name}`;
-            },
-            defaultSizes(){
-                return this.slot.defaultSizes;
-            }
         },
     }
 </script>
